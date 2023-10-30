74caa81f06bc4a9882b3aecf7a0cd11faf0224ac
carlosgl20 <carlosgl20>
2023-10-30 13:21:13 +0100

    DWEC_UNI2_PRAC2

--- ejercicio1/ejercicio1.js
+++ ejercicio1/ejercicio1.js
@@ -1,6 +1,14 @@
-function separaNumeros() {
-const numerosEnteros = [];
-for () {
-    
-} 
-}
+function separaNumeros(...parametros) {
+    const numerosEnteros = [];
+  
+    for (const parametro of parametros) {
+      if (typeof parametro == 'number' && Number.isInteger(parametro)) {
+        numerosEnteros.push(parametro);
+      }
+    }
+  
+    return numerosEnteros;
+  }
+  
+  const numeros = separaNumeros(4, 'Hola', 6, 4.12, 'Gente', 6);
+  console.log(numeros);
